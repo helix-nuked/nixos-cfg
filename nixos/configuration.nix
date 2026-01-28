7d7f8fddda86d43be1a2d70748bf5c9d32e91cfd
helix-nuked <helix.nuked@proton.me>
2026-01-28 18:33:28 +0500

    Test mutableUsers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -601,7 +601,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
-    mutableUsers = true;
+    mutableUsers = false;
     users = {
       monyx = {
         isNormalUser = true;
